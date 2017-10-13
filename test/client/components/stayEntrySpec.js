8c9c5f2aa2e8aa20d5cd589354ff508b76eb0e19
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-13 08:48:53 -0700

    Ignore snippets and add stays todos

---
+++ test/client/components/stayEntrySpec.js
@@ -0,0 +1,22 @@
+
+
+
+  //todo: rendered ok
+  //todo: is type 'div'
+  //todo: Renders with stay details: className = "stayDetail"
+  // Date: ${moment(stay.startDate).calendar()}
+  // to ${moment(stay.endDate).calendar()}`}
+  // {`Price Per Night: $${stay.pricePer}`}
+  // {`Total: $${stay.totalPrice}`}
+
+
+  //todo: make role 'guest', populate with a new stay. Try to cancel.
+  //todo: make role 'guest', populate with a new stay, set status to 'complete'. Try to cancel.
+
+  //todo: handle put to '/api/stay/cancel/' + this.props.stay._id , headers: {'authorization': this.token}, status: 'cancelled'
+  //todo: handle put to '/api/stay/approve/' + this.props.stay._id,, headers: {'authorization': this.token}, status: 'confirmed'
+  //todo: handle put to /api/stay/reject/ + stayId, headers: {'authorization': this.token}, status: 'rejected'
+
+
+  //todo: make role 'host', populate with a new stay. Try to reject.
+  //todo: make role 'host', populate with a new stay, set status to 'complete'. Try to review.
