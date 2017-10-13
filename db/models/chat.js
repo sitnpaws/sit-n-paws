429f0fecde5ebe5ba93e33c45155bd6c0039d73d
Jared Meek <jmeek13@gmail.com>
2017-10-12 17:34:57 -0700

    added in chat schema

--- db/models/chat.js
+++ db/models/chat.js
@@ -4,10 +4,22 @@ const messageSchema = new mongoose.Schema(
   {
     text: { type: String, required: true },
     user: { type: mongoose.schema.ObjectId, required: true, ref: 'User'},
-    conversationId: { type: mongoose.schema.ObjectId, required: true }
+    chatId: { type: mongoose.schema.ObjectId, required: true }
   },
   { timestamps: true }
 );
 
-const Msg = mongoose.model('Message', messageShema);
+const Msg = mongoose.model('Message', messageSchema);
 module.exports.Msg = Msg;
+
+const chatSchema = new mongoose.Schema(
+  {
+    stay: { type: mongoose.schema.ObjectId, required: true, ref: 'Stay'}
+    host: { type: mongoose.schema.ObjectId, required: true, ref: 'User'},
+    guest: { type: mongoose.schema.ObjectId, required: true, ref: 'User'},
+    count: { type: Number, default: 0 }
+  }
+);
+
+const Chat = mongoose.model('Conversation', chatSchema);
+module.exports.Chat = Chat;
