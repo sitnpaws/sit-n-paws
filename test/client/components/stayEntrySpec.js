29c0fa7b130cc83782f3b61c63fae4c24dfff537
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-10-13 10:37:24 -0700

    Refactor todos

--- test/client/components/stayEntrySpec.js
+++ test/client/components/stayEntrySpec.js
@@ -25,16 +25,20 @@ const stay = {
   // {`Total: $${stay.totalPrice}`}
 
 
+
+
   //todo: make role 'guest', populate with a new stay. Try to cancel.
-
-
   //todo: handle put to '/api/stay/cancel/' + this.props.stay._id , headers: {'authorization': this.token}, status: 'cancelled'
+
+  //todo: make role 'host', populate with a new stay. Try to approve.
   //todo: as role: 'host', handle put to '/api/stay/approve/' + this.props.stay._id,, headers: {'authorization': this.token}, status: 'confirmed'
+
+  //todo: make role 'host', populate with a new stay. Try to reject.
   //todo: as role: 'host', handle put to /api/stay/reject/ + stayId, headers: {'authorization': this.token}, status: 'rejected'
 
 
-  //todo: make role 'host', populate with a new stay. Try to reject.
-  //todo: make role 'host', populate with a new stay. Try to approve.
+
+
 
 
 //Review not yet implimented
