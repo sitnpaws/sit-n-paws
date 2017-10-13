9107e41c72b7964943c15a42b01af3cf0bcee3c8
Jared Meek <jmeek13@gmail.com>
2017-10-12 20:03:51 -0700

    adding back in ...props so that it will work normally with path params

--- src/client/components/private.js
+++ src/client/components/private.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 // Redirects to login if checkAuth returns false, else allows rendering of Main
-const PrivateRoute = ({ component: Component, checkAuth }) => (
-  <Route render={(props) => (
+const PrivateRoute = ({ component: Component, checkAuth, ...rest }) => (
+  <Route {...rest} render={(props) => (
     checkAuth() ? (
       <Component {...props}/>
     ) : (
