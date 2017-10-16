571f0b4d4d807062630d3e6b58190f0dcb7a491f
Jacob Yoo <jacobyoo@gmail.com>
2017-10-15 17:26:33 -0700

    modified for more intelligent sweep

--- stay-sweeper.js
+++ stay-sweeper.js
@@ -1,22 +1,21 @@
 const mongoose = require ('mongoose');
+const sitnpaws = require('./db/config');
 mongoose.Promise = global.Promise;
 const Stay = require('./db/models/stays');
 
 let today = new Date();
 
-const sweepStays = (() => {
-  Stay.find({}).where('endDate').lt(today).exec().then((stays) => {
-    stays.forEach((stay) => {
-      if (stay.status === 'pending') {
-        stay.update({'status': 'expired'}).exec();
-      } else if (stay.status === 'approved') {
-        stay.update({'status': 'complete'}).exec();
-      }
-    });
+const sweepStays = () => {
+  const expiringStays = Stay.find({}).where('startDate').gt(today).where('status').equals('pending').exec();
+  const completeStays = Stay.find({}).where('endDate').gt(today).where('status').equals('approved').exec();
+
+  Promise.all([expiringStays, completeStays]).then(([expiringStays, completeStays]) => {
+    expiringStays.forEach(stay => stay.update({'status': 'expired'}).exec());
+    completeStays.forEach(stay => stay.update({'status': 'complete'}).exec());
   }).catch((err) =>{
     console.log(err);
   });
   console.log('sweeping!');
-});
+};
 
 sweepStays();
