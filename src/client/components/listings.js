ac64628db65c77c28164f5a6012a70b6c27d0aef
Jared Meek <jmeek13@gmail.com>
2017-10-15 20:27:23 -0700

    load listings on mount and remove console log

--- src/client/components/listings.js
+++ src/client/components/listings.js
@@ -14,11 +14,11 @@ export default class Listings extends React.Component {
     this.handleSearch = this.handleSearch.bind(this);
   }
 
+  componentDidMount() { this.handleSearch(''); }
   // Search - live search by zipcode
   handleSearch(term) {
     axios.get(`/api/listings/${term}`, {headers: {'authorization': this.props.getToken()}})
       .then(resp => {
-        console.log('listings search response: ', response);
         this.setState({ listings: resp.data});
       }).catch(err => console.log('Listings search error: ', err));
       // holding onto this until i can get to testing -> const url = `/listings/${term}`; request.get(url, (err, res) => {  if (err) {  console.log(err); } else { this.setState({ listings:res.body }); } });
@@ -31,7 +31,7 @@ export default class Listings extends React.Component {
           <Search onChange={this.handleSearch}/>
         </div>
         <div className="wrapper">
-          {this.state.listings.map((listing, i) => (<ListingEntry listing={listing} key={listing.name} />))}
+          {this.state.listings.map((listing, i) => (<ListingEntry getToken={this.props.getToken} listing={listing} key={listing.name} />))}
           {this.state.listings.length === 0 ? <div className="messageBox"><h2><em>Please Try A Different Zipcode</em></h2></div> : ''}
         </div>
       </div>
