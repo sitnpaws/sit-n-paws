eea195888a5882fab8a6e4edcefa6db34e444768
Jared Meek <jmeek13@gmail.com>
2017-10-15 20:22:49 -0700

    rebuilt toolbar as custom toolbar

--- src/client/components/main-app-bar.js
+++ src/client/components/main-app-bar.js
@@ -1,43 +1,28 @@
 // libraries
 import React from 'react';
-
-// material ui components
-import AppBar from 'material-ui/AppBar';
-import { Tabs, Tab } from 'material-ui/Tabs';
-import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
-import Pets from 'material-ui/svg-icons/action/pets';
+import { Link } from 'react-router-dom';
 
 // styles
-const styles = {
-  appBar: {
-    background: '#C5BA9B',
-    alignItems: 'center',
-    overflow: 'hidden',
-    position: 'fixed',
-    top: 0,
-    left: 0
-  },
-  tabItemContainer: { background: 'none', maxWidth: '600px' },
-  tab: { backgroundColor: '#C5BA9B', height: '40px', marginTop: '25px', marginBottom: 0 },
-  title: { background: '#C5BA9B', height: 'auto', lineHeight: 'auto' }
-}
+import './main-app-bar.css';
 
 export default class MainAppBar extends React.Component {
   render() {
     return (
-      <AppBar
-        title={
-          <Tabs tabItemContainerStyle={styles.tabItemContainer} inkBarStyle={{background: 'none'}}>
-            <Tab label="Listings" value="listings" style={styles.tab} href="/listings"/>
-            <Tab label="Stays" value="stays" style={styles.tab} href="/stays"/>
-          </Tabs>
-        }
-        iconElementLeft={<IconButton><Pets/></IconButton>}
-        iconElementRight={<IconButton><NavigationMenu/></IconButton>}
-        onRightIconButtonTouchTap={this.props.toggleDrawer} style={styles.appBar}
-      >
-      </AppBar>
-    )
+      <div className="main-app-bar-wrapper">
+        <div className="main-app-bar-left-group">
+          <div className="main-app-bar-icon"><i className="material-icons">pets</i></div>
+          <div className="main-app-bar-title"><span>Sit-N-Paws</span></div>
+        </div>
+        <div className="main-app-bar-nav-group">
+          <div className="main-app-bar-link-div"><Link to="/listings">Listings</Link></div>
+          <div className="main-app-bar-link-div"><Link to="/stays">Stays</Link></div>
+        </div>
+        <div className="main-app-bar-right-group" onClick={this.props.toggleDrawer}>
+          <div className="main-app-bar-icon">
+            <i className="material-icons">menu</i>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
