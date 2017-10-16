0e4ea00c80cdf5c708d4c68f550496ab886494d7
Jared Meek <jmeek13@gmail.com>
2017-10-15 18:13:09 -0700

    general cleanup and refactor

--- src/client/components/stays.js
+++ src/client/components/stays.js
@@ -1,65 +1,30 @@
+// libraries
 import React from 'react';
 import axios from 'axios';
+
+// app components / modules
 import StayEntry from './stayEntry.js';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
-import Pets from 'material-ui/svg-icons/action/pets';
-import RaisedButton from 'material-ui/RaisedButton';
-import jwt from 'jsonwebtoken';
 
-import {
-  blue300,
-  indigo900,
-  orange200,
-  deepOrange300,
-  pink400,
-  purple500,
-} from 'material-ui/styles/colors';
-
-class Stays extends React.Component {
+export default class Stays extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      stays: []
-    };
+    this.state = { stays: [] };
   }
 
-  componentWillMount() {
-    this.token = localStorage.jwt;
-  }
-
-  componentDidMount() {
-    this.getStays();
-  }
+  componentDidMount() { this.getStays(); }
 
   getStays() {
-    axios.get('/api/stays', { headers: {'Authorization': this.token} })
+    axios.get('/api/stays', { headers: {'Authorization': this.props.getToken()}})
       .then((res) => {
-        this.setState({
-          stays: res.data
-        })
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        this.setState({ stays: res.data });
+      }).catch((err) => console.log(err));
   }
 
   render() {
     if (this.state.stays.length === 0) {
-      return (
-        <div>
-          You have no stays yet! Woof!
-        </div>
-      )
+      return <div>You have no stays yet! Woof!</div>;
     } else {
-      return (
-        this.state.stays.map(stay => <StayEntry stay={stay} key={stay._id}/>)
-      )
+      return (this.state.stays.map(stay => <StayEntry stay={stay} key={stay._id}/>));
     }
   }
 }
-
-
-export default Stays;
